96a4266975f666119001042c7e9cae60f0ab6838
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2024-01-14 08:56:45 +0300

    add import alias for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://localhost:5173/'
-})
+  base: "http://localhost:5173/",
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+});
